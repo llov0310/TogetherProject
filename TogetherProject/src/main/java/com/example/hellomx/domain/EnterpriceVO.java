78ba3d645be3bda0745856af3aed03b25ba5752a
llov0310 <46309820+llov0310@users.noreply.github.com>
2019-04-01 21:43:52 +0900

    4/1 hotel 부분 지도 api 삽입 완료

---
+++ TogetherProject/src/main/java/com/example/hellomx/domain/EnterpriceVO.java
@@ -0,0 +1,15 @@
+package com.example.hellomx.domain;
+
+import lombok.Data;
+
+@Data
+public class EnterpriceVO {
+	
+	private String enterprice_code;
+	private String enterprice_number;
+	private String address;
+	private String enterprice_name;
+	private String user_id;
+	private String phone_number;
+	private String intro;
+}
