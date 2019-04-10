e7ead00efa519c16b8705a364805550c129c2c64
llov0310 <46309820+llov0310@users.noreply.github.com>
2019-04-10 20:37:12 +0900

    4/10 hotel 페이지 웹부분 구현중

--- TogetherProject/src/main/webapp/resources/assets/js/hotel.js
+++ TogetherProject/src/main/webapp/resources/assets/js/hotel.js
@@ -1,4 +1,21 @@
-
   function mapserch(){
 	  location.href = "/hotelserch"
   }
+  
+$(document).ready(function() {
+     
+      $("#searchbar").click(function() {
+    	  
+         $("#searchlist").toggle(); //천천히 보이기
+
+      });
+   }); 
+
+
+
+function place(text){
+
+	var a = text;
+	$("#searchbar").val(a);
+	$("#searchlist").hide();
+}
