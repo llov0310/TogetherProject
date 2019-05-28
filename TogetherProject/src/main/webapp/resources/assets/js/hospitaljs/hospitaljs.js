7d94956e09187c9532ccd67ea503758240453a36
llov0310 <46309820+llov0310@users.noreply.github.com>
2019-05-28 11:18:13 +0900

    05/28 공공데이터 삽입 완료

--- TogetherProject/src/main/webapp/resources/assets/js/hospitaljs/hospitaljs.js
+++ TogetherProject/src/main/webapp/resources/assets/js/hospitaljs/hospitaljs.js
@@ -6,8 +6,41 @@ $(document).ready(function() {
 		type : "GET",
 		dataType : 'json',
 		success : function(data){
-			alert("성공");
-			console.log(data);
+			
+			var test = Object.values(data);
+			
+			var cont_len = test[0].length;
+			
+//			console.log(Object.values(test[0]));//배열 전체
+//			console.log(Object.values(test[0][0]/* 부분배열  ex)[0],[1],[2],[3]*/)); // 한 배열의 내용
+//			console.log(Object.values(test[0][0])[0]/*원하는 String 값*/); // 그 배열안의 인덱스
+			
+			for(var i=0; i<cont_len; i++){
+				
+					
+					$('#main1-2').
+					append('<div class="listbox1">'
+							+ '<div class="listbox2">'
+							+ '<div id="imgbox1"><div id="img1"><img src="http://placehold.it/200x200" alt="" id="imgbox">'
+							+ '</div></div>'
+							+ '<div class="to">'
+							+ '<div class="context"><div><div>'
+							+ '<h2>' + Object.values(test[0][i])[1] + '</h2></div>'
+							+ '<div class="ok">' + Object.values(test[0][i])[2] 
+							+ '</div>'
+							+ '<div>'+ Object.values(test[0][i])[3] +'</div>'
+							+ '</div></div>'
+							+ '<div id="context1"><div>'
+							+ '<div>진료시간</div><div>오전10시~ 오후 9시까지</div></div></div>'
+							//여기서부터 지도
+							+ '<div class="fow">위치틀<div>'
+							+ '<div>위치 <span class="off">닫기</span></div>'
+							+ '<div id="map" class="map" style="width:1000px;height:500px;"></div>'
+							+ '</div></div></div></div></div>');
+			
+				
+			}
+
 			}
 		});
 	
@@ -18,12 +51,8 @@ $(document).ready(function() {
 		 });
 
 
-
-			$(".ok").click(function() {
-			var a =$(this).parent().parent().parent();
-			a.children().eq(3).toggle();
-			map.relayout()
-			 
+		 $('.listbox1').on('click',function(){
+//			$(this). 
 		 });
 
 	
