a5af01bc537fe10aad6ee7c6ccc7db32bf1f8dc6
KimJongEon <45247057+KimJongEon@users.noreply.github.com>
2019-06-01 17:00:07 +0900

    06/01 종언
    파이어베이스 json파일 읽어오는 경로문제 100% 해결

--- TogetherProject/src/main/java/com/together/controller/SignController.java
+++ TogetherProject/src/main/java/com/together/controller/SignController.java
@@ -1,10 +1,15 @@
 package com.together.controller;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,20 +37,61 @@ public class SignController {
 
 	// 회원가입
 	@RequestMapping(value = "/sign", method = RequestMethod.POST)
-	public String signup(Model model, MemberVO ins) throws IOException {
+	public String signup(Model model, MemberVO ins, HttpServletRequest request) throws IOException {
 		String user_id = ins.getUser_id();
+		String password = ins.getPassword();
+		String user_nm = ins.getUser_nm();
 		
+//		private String ph_no;
+//		private String email;
+//		private String user_nm;
+//		private Timestamp register_dt;
+//		private int authority_no;
+//		private String birth_dt;
+//		private int sign_no;
+//		private String addr_ji;
+//		private String addr_dong;
+		
+		String path = request.getSession().getServletContext().getRealPath("/");
+//		System.out.println(path + "resources\\firebase\\blogapp-a9a56-firebase-adminsdk-v8z9o-c7af607772.json");
+
+		// 서버 올릴 때 경로
+		String firebasePath = path + "resources"+ File.separator +"firebase" + File.separator + "blogapp-a9a56-firebase-adminsdk-v8z9o-c7af607772.json";
 		
 		//▼▼▼파이어베이스▼▼▼
-		FileInputStream serviceAccount = new FileInputStream("C:\\firebase/blogapp-a9a56-firebase-adminsdk-v8z9o-c7af607772.json");
-		//resources/firebase/blogapp-a9a56-firebase-adminsdk-v8z9o-c7af607772.json
-		FirebaseOptions options = new FirebaseOptions.Builder()
-		    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-		    .setDatabaseUrl("https://blogapp-a9a56.firebaseio.com")
-		    .build();
+		FileInputStream serviceAccount = new FileInputStream(firebasePath);
+		
+		
+		
+		// /resources/firebase/blogapp-a9a56-firebase-adminsdk-v8z9o-c7af607772.json
+		// C:\\firebase/blogapp-a9a56-firebase-adminsdk-v8z9o-c7af607772.json
+//		FirebaseOptions options = new FirebaseOptions.Builder()
+//		    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//		    .setDatabaseUrl("https://blogapp-a9a56.firebaseio.com")
+//		    .build();
+//
+//		FirebaseApp.initializeApp(options);
 
-		FirebaseApp.initializeApp(options);
+		
+		// 수정 코드
+		FirebaseApp firebaseApp = null;
+		List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
+		if (firebaseApps != null && !firebaseApps.isEmpty()) {
+			for (FirebaseApp app : firebaseApps) {
+				if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+					firebaseApp = app;
+				}
+			}
+		} else {
+			FirebaseOptions options = new FirebaseOptions.Builder()
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.setDatabaseUrl("https://blogapp-a9a56.firebaseio.com").build();
 
+			FirebaseApp.initializeApp(options);
+		}
+		
+		
+		
 		// As an admin, the app has access to read and write all data, regardless of Security Rules
 		DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Member");
 
@@ -54,7 +100,7 @@ public class SignController {
 		  @Override
 		  public void onDataChange(DataSnapshot dataSnapshot) {
 		    Object document = dataSnapshot.getValue();
-		    System.out.println(document);
+		    System.out.println("오브젝트 도큐먼트 확인 : " + document);
 		  }
 
 		  @Override
@@ -66,13 +112,13 @@ public class SignController {
 		DatabaseReference useridRef = null;
 		useridRef = ref.child(user_id);
 		
-		Map<String, Object> Map = new HashMap<>();
+		Map<String, Object> Map = new HashMap<>(); // 맵 생성하고
 		Map.put("user_id", user_id);
+		Map.put("password", password);
+		Map.put("user_nm", user_nm);
 		
 		useridRef.setValueAsync(Map);
-		
-		//useridRef.child("user_id").setValueAsync(user_id);
-//		useridRef.setValueAsync(user_id);
+
 		
 		System.out.println("파베 테스트 : "+ Map );
 		
